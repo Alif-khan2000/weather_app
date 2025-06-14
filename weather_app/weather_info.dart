b985494439f085fa6bfba356f797fb101a9a18f8
Alif-khan2000 <alifkhan.n2000@gmail.com>
2025-06-14 20:03:23 +0530

    final commit

--- weather_app/weather_info.dart
+++ weather_app/weather_info.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class weather_info extends StatelessWidget {
   final String name;
@@ -7,6 +8,7 @@ class weather_info extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double w = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Stack(
         children: [
@@ -20,6 +22,21 @@ class weather_info extends StatelessWidget {
               },
               backgroundColor: Colors.blueGrey,
               child: Icon(Icons.arrow_back, color: Colors.white),
+            ),
+          ),
+          Positioned(
+            left: 110,
+            bottom: 50,
+            child: Container(
+              width: w * .62,
+              height: 55,
+              decoration: BoxDecoration(
+                color: Colors.blueGrey,
+                borderRadius: BorderRadius.circular(60),
+              ),
+              child: Center(
+                child: Text(name,style: GoogleFonts.aBeeZee(color: Colors.white),),
+              ),
             ),
           ),
         ],
